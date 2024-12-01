e348c255de6d00edb8aae923d24cad6c9076cb23
Kimberley09 <kimberleyc8@nycstudents.net>
2024-12-01 13:48:09 -0500

    dasf

---
+++ app/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './index.html',
+      './app/*/.{js,css}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };  
---
+++ app/vite.config.cjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+  css: {
+    postcss: './postcss.config.js',
+  },
+});
